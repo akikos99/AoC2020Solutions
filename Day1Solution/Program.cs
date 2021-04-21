e782e8076e2af103be31efd3ca88678b6afc12b8
akikos99 <54589767+akikos99@users.noreply.github.com>
2021-04-21 11:56:04 -0400

    Add files via upload

---
+++ Day1Solution/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace Day1Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AdventOfCodeDay1Input.txt"));
+            List<int> expenses = (Array.ConvertAll(input, s => Int32.Parse(s))).ToList();
+            expenses.Sort();
+
+            Console.WriteLine($"Part one solution: {PartOne(expenses)}\n");
+            Console.WriteLine($"Part one solution: {PartTwo(expenses)}\n");
+        
+        }
+
+        public static int PartOne(List<int> List) 
+        {
+            foreach (int i in List)
+            {
+                foreach (int y in List)
+                {
+                    if (i + y == 2020)
+                    {
+                        return i * y;
+                    }
+                }
+            }
+
+        return 0;
+        }
+
+        public static int PartTwo(List<int> List) 
+        {
+            foreach (int i in List)
+            {
+                foreach (int y in List)
+                {
+                    foreach (int z in List)
+                    {
+                        if (i + y + z == 2020)
+                        {
+                            return i * y * z;
+                        }
+                    }
+                    
+                }
+            }
+
+        return 0;
+        }
+    }
+}
---
+++ Day2Solution/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace Day2Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AoCDay2Input.txt"));
+
+            Console.WriteLine($"Part one solution: {PartOne(input)}");
+            Console.WriteLine($"Part two solution: {PartTwo(input)}");
+        }
+
+
+        public static int PartOne(string[] input) 
+        {
+            int accepted = 0;
+
+            foreach (string word in input)
+            {
+                int count = 0;
+                string[] semiColonSplit = word.Split(":"); //separates string into two parts, before and after the :
+                string password = semiColonSplit[1];
+                string[] spaceSplit = semiColonSplit[0].Split(" "); //separates string into letter and min/max
+                string letter = spaceSplit[1];
+                string[] dashSplit = spaceSplit[0].Split("-"); //separates string into min and max
+                string min = dashSplit[0]; //lower amount of char
+                string max = dashSplit[1]; //upper amount of char
+
+                //count = password.Count(f => f == letter);
+                count = password.Split(letter).Length - 1;
+
+                if (count >= int.Parse(min) && count <= int.Parse(max))
+                {
+                    accepted++; // = accepted + 1;
+                }
+            }
+           
+           return accepted;
+        }
+
+        public static int PartTwo(string[] input) 
+        {
+            int accepted = 0;
+
+            foreach (string word in input)
+            {
+
+                string[] semiColonSplit = word.Split(":"); //separates string into two parts, before and after the :
+                string password = semiColonSplit[1];
+                string[] spaceSplit = semiColonSplit[0].Split(" "); //separates string into letter and min/max
+                string letter = spaceSplit[1];
+                string[] dashSplit = spaceSplit[0].Split("-");
+                string posOne = dashSplit[0]; //lower amount of char
+                string posTwo = dashSplit[1]; //upper amount of char
+
+
+                string posOneChar = password.Substring(int.Parse(posOne), 1);
+                string posTwoChar = password.Substring(int.Parse(posTwo), 1);
+
+                //System.Console.WriteLine(letter);
+
+                if ((posOneChar == letter && posTwoChar != letter) || (posOneChar != letter && posTwoChar == letter))
+                {
+                    accepted++; // = accepted + 1;
+                }
+            }
+           
+           return accepted;
+        }
+        
+    }
+}
