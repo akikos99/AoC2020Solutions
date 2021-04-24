864d08a9a97551d7ac81ed9700e977e8a83203e8
akikos99 <54589767+akikos99@users.noreply.github.com>
2021-04-24 12:19:47 -0400

    Add files via upload

---
+++ Day6Solution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+//First trry to push to git
+
+namespace Day6Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+//New Method
+public static int PartOne(string[] input) 
+{
+    
+}
---
+++ Day8Solution/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+//Extraordinarily long method of solving, however
+
+namespace Day8Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AoCDay8Input.txt"));
+
+            Console.WriteLine($"Part one solution: {PartOne(input)}");
+            //Console.WriteLine($"Part two solution: {PartTwo(input)\n}");
+        }
+
+
+        public static int PartOne(string[] input) 
+        {
+            //just need to figure out where the infinite loop starts.
+            int acc = 0;
+            //int count = 0;
+            bool test = true;
+            int index = 0;
+
+            while (test == true)
+            {
+                string[] spaceSep = input[index].Split(" ");
+                string operation = spaceSep[0];
+                string argument = spaceSep[1];
+
+                //System.Console.WriteLine(operation);
+                //int index = 0;
+
+                if (operation == "acc")
+                {
+                    acc = acc + int.Parse(argument);
+                    index ++;
+                }
+
+                else if (operation == "jmp")
+                {
+                    //jump spaceSep[1] amount
+                    index = index + int.Parse(argument);
+
+                    //word.Skip(int.Parse(argument));
+                }
+
+                else
+                {
+                    //do nothing
+                    index ++;
+                }
+            }
+            return acc;
+        }
+
+        // public static int SolvePartTwo(string[] input) 
+        // {
+
+        //     foreach (string word in input)
+        //     {
+        //     }
+
+        // }
+        
+    }
+}
