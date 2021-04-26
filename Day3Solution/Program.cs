a50100dce7625ca54c63a793de0392b3b4d9bf0b
akikos99 <akikos@purdue.edu>
2021-04-26 11:48:30 -0400

    Still Stuck

--- Day3Solution/Program.cs
+++ Day3Solution/Program.cs
@@ -8,13 +8,13 @@ using System.Linq;
 
 namespace Day3Solution
 {
-    class Program
+    class Program3
     {
         static void Main(string[] args)
         {
+            //var input = (File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AoCDay3Input.txt"))).ToList();
+            //var input = inputS.ToList();
             string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AoCDay3Input.txt"));
-
-            
             Console.WriteLine($"Part one solution: {PartOne(input)}\n");
             //Console.WriteLine($"Part two solution: {PartTwo(input)}\n");
         }
@@ -22,23 +22,42 @@ namespace Day3Solution
 
         public static int PartOne(string[] input) 
         {
-            int row;
             int tree = 0;
 
-            foreach (string line in input)
-            {
-                int column = 4;
+            // foreach (string line in input)
+            // {
+                int lineLength = input[0].Length;
+                //System.Console.WriteLine(lineLength);
+                int column = 0;
+                int row = 0;
                 
-                for (row = 0; row < 11; row++)
+                while (row != lineLength - 1)
                 {
-                    if (line.Substring(column, 1) == "#")
+                    row++;
+                    column = column + 3;
+                    
+
+                    if (column >= lineLength)
                     {
-                        column = column + 3;
+                        column = column % lineLength;
+                    }
+
+                     if (input[row].Substring(column, 1)  == "#")
+                    {
                         tree++;
                     }
+
                 }
+                //for (row = 0; row < 11; row++) //No longer needed because I just need to extend to the right
+                // {
+                //     if (line.Substring(column, 1) == "#")
+                //     {
+                //         column = column + 3;
+                //         tree++;
+                //     }
+                // }
                 
-            }
+           // }
            return tree;
         }
 
