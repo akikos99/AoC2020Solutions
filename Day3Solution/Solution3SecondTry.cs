c926d2bc2fa2386127aba18e2ea1ec4e1a0a5152
akikos99 <akikos@purdue.edu>
2021-04-26 19:23:38 -0400

    Final

--- Day3Solution/Solution3SecondTry.cs
+++ Day3Solution/Solution3SecondTry.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 namespace Day3Solution
 {
-    class Program
+    class program4
     {
         static void Main(string[] args)
         {
@@ -20,30 +20,38 @@ namespace Day3Solution
         }
 
 
-        public static int PartOne(string[] input) 
+         public static int PartOne(string[] input) 
         {
-            int row;
-            int tree = 0;//
+            int trees = 0;
 
-            foreach (string line in input)
-            {
-                int lineLength = line.Length();
-                int column = 1;
-                
-                do
+            // foreach (string line in input)
+            // {
+                int lineLength = input[0].Length;
+                int row = 0;
+                int column = 0;
+                //System.Console.WriteLine(lineLength);
+
+                while (row != input.Length - 1)
                 {
-                    if (line.Substring(column, 1) == "#")
-                    {
-                        column = column + 3;
-                        tree++;
-                    }
+                    row++;
+                    column += 3;
 
-                    if (column > lineLength)
+                    if (column >= lineLength)
                     {
                         column = column % lineLength;
                     }
-                }
-                while (line != null);
+
+                    if (input[row].Substring(column, 1)  == "#")
+                    {
+                        trees++;
+                    }
+            }
+
+
+            return trees;
+        }
+
+                // }
                 //for (row = 0; row < 11; row++) //No longer needed because I just need to extend to the right
                 // {
                 //     if (line.Substring(column, 1) == "#")
@@ -53,9 +61,8 @@ namespace Day3Solution
                 //     }
                 // }
                 
-            }
-           return tree;
-        }
+           // }
+           //return trees;
 
         
     }
