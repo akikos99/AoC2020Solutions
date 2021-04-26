55f234f20f35320fa69cc72e2433dc5164e70a73
akikos99 <akikos@purdue.edu>
2021-04-26 15:48:09 -0400

    Part1 Finished

--- Day3Solution/Program.cs
+++ Day3Solution/Program.cs
@@ -20,34 +20,38 @@ namespace Day3Solution
         }
 
 
-        public static int PartOne(string[] input) 
+        private static int PartOne(string[] input) 
         {
-            int tree = 0;
+            int trees = 0;
 
             // foreach (string line in input)
             // {
                 int lineLength = input[0].Length;
-                //System.Console.WriteLine(lineLength);
-                int column = 0;
                 int row = 0;
-                
-                while (row != lineLength - 1)
+                int column = 0;
+                //System.Console.WriteLine(lineLength);
+
+                while (row != input.Length - 1)
                 {
                     row++;
-                    column = column + 3;
                     
+                    column += 3;
 
                     if (column >= lineLength)
                     {
                         column = column % lineLength;
                     }
 
-                     if (input[row].Substring(column, 1)  == "#")
+                    if (input[row].Substring(column, 1)  == "#")
                     {
-                        tree++;
+                        trees++;
                     }
+            }
 
-                }
+            return trees;
+        }
+
+                // }
                 //for (row = 0; row < 11; row++) //No longer needed because I just need to extend to the right
                 // {
                 //     if (line.Substring(column, 1) == "#")
@@ -58,8 +62,7 @@ namespace Day3Solution
                 // }
                 
            // }
-           return tree;
-        }
+           //return trees;
 
         
     }
