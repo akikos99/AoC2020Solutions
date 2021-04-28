f610cfcbbbf6d41ff9543d3d20f4623be60b7605
akikos99 <akikos@purdue.edu>
2021-04-27 23:21:48 -0400

    Haven't been able to create list yet, but validation function looks good

--- Day4Solution/Program.cs
+++ Day4Solution/Program.cs
@@ -11,24 +11,58 @@ namespace Day4Solution
     {
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AoCDay4Input.txt"));
-
-            Console.WriteLine($"Part one solution: {PartOne(input)}\n");
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AoCDay4Solution.txt"));
+            List<string> inputs = CreateList(input);
+            Console.WriteLine($"Part one solution: {PartOne(inputs)}\n");
             //Console.WriteLine($"Part two solution: {PartTwo(input)}\n");
         }
 
+        
 
-        public static int PartOne(string[] input) 
+        public static List<string> CreateList(string[] input)
+        {
+           List<string> inputList = new List<string>();
+        
+           foreach (string person in input)
+           {
+               if (person == "\n")
+               {
+                   inputList.Add(person);
+               }
+               System.Console.WriteLine(inputList);
+           }
+           return inputList;
+        }
+        
+        public static int PartOne(List<string> input) 
         {
             //Need to split by \n for each separate person
             //so maybe 
-            foreach (string line in input)
+            int total = 0;
+
+            foreach (string attributes in input)
             {
-                do 
+                bool contBYR = attributes.Contains("byr");
+                bool contIYR = attributes.Contains("iyr");
+                bool contEYR = attributes.Contains("eyr");
+                bool contHGT = attributes.Contains("hgt");
+                bool contHCL = attributes.Contains("hcl");
+                bool contECL = attributes.Contains("ecl");
+                bool contPID = attributes.Contains("pid");
+                bool contCID = attributes.Contains("cid");
+                
+                if (contBYR == true && contIYR == true && contEYR == true && contHGT == true && contHCL == true && contECL == true && contPID == true)
                 {
-                    int hat = 1;
-                } while (line != null);
+                    total += 1;
+                }
+                else
+                {
+                    
+                }
+        
             }
+
+            return total;
             
         }
 
